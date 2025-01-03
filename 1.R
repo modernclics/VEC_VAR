b31f9df24ff394dd7ded0e1ca1e2e27fa9c89ea8
modernclics <155316363+modernclics@users.noreply.github.com>
2025-01-02 19:34:26 -0500

    Add files via upload

---
+++ 1.R
@@ -0,0 +1,52 @@
+N <- 500
+a <- 1
+l <- 0.01
+rho <- 0.7
+
+set.seed(246810)
+v <- ts(rnorm(N,0,1))
+
+par(mfrow = c(3,2))
+
+y <- ts(rep(0,N))
+for (t in 2:N){
+  y[t]<- rho*y[t-1]+v[t]
+}
+plot(y,type='l', ylab="rho*y[t-1]+v[t]",main= "Sin tendencia")
+abline(h=0)
+
+y <- ts(rep(0,N))
+for (t in 2:N){
+  y[t]<- a+rho*y[t-1]+v[t]
+}
+plot(y,type='l', ylab="a+rho*y[t-1]+v[t]", main= "Con constante")
+abline(h=0)
+
+y <- ts(rep(0,N))
+for (t in 2:N){
+  y[t]<- a+l*time(y)[t]+rho*y[t-1]+v[t]
+}
+plot(y,type='l', ylab="a+l*time(y)[t]+rho*y[t-1]+v[t]", main = "Con tendencia y constante")
+abline(h=0)
+
+y <- ts(rep(0,N))
+for (t in 2:N){
+  y[t]<- y[t-1]+v[t]
+}
+plot(y,type='l', ylab="y[t-1]+v[t]", main = "Caminata aleatoria")
+abline(h=0)
+
+a <- 0.1
+y <- ts(rep(0,N))
+for (t in 2:N){
+  y[t]<- a+y[t-1]+v[t]
+}
+plot(y,type='l', ylab="a+y[t-1]+v[t]", main = "Caminata aleatoria con constante")
+abline(h=0)
+
+y <- ts(rep(0,N))
+for (t in 2:N){
+  y[t]<- a+l*time(y)[t]+y[t-1]+v[t]
+}
+plot(y,type='l', ylab="a+l*time(y)[t]+y[t-1]+v[t]", main = "Caminata aleatoria con constante y tendencia")
+abline(h=0)
